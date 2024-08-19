03e11ed1778d1b443dbd6d82844e89477f864295
Joao-Paulo-Bortolocce <149070264+Joao-Paulo-Bortolocce@users.noreply.github.com>
2024-08-19 10:05:40 -0300

    Aprendendo sobre state

--- src/meusCOmponentes/Data.js
+++ src/meusCOmponentes/Data.js
@@ -5,23 +5,33 @@ export default class Data extends Component{
     constructor(props){
         super();
         this.props= props;
-        this.dataAtual = new Date().toLocaleString();
+        this.state = {
+            dataAtual : new Date().toLocaleString()
+        }
     }
-
+    
     //fase de montagem
     componentDidMount(){
         console.log("O componente foi montado.");
-        this.dataAtual=  new Date().toLocaleString();
+        //Não é permitido atualizar o estado componente de forma direta "this.state=.."
+        this.setState({
+            dataAtual: new Date().toLocaleString()
+        })
     }
-
+    
     componentDidUpdate(){
         console.log("O compontente foi atualizado.");
+        setTimeout(()=>{
+            this.setState ( {
+                dataAtual : new Date().toLocaleString()
+            })
+        },1000);
     }
 
     //Sobrescrita de método
     render(){
         return(
-            <h1>{this.props.texto || ""}{this.dataAtual}</h1>
+            <h1>{this.props.texto || ""}{this.state.dataAtual}</h1>
         )
     }
 }
